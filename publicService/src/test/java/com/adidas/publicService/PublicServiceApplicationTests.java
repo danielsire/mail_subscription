13bd7c88d10f9a75f9921cafe85ef88b9248e0f9
Daniel Sire <sirehorta@gmail.com>
2021-05-11 00:11:50 +0200

    fix configuration

--- publicService/src/test/java/com/adidas/publicService/PublicServiceApplicationTests.java
+++ publicService/src/test/java/com/adidas/publicService/PublicServiceApplicationTests.java
@@ -4,10 +4,12 @@ package com.adidas.publicService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("tests")
 public class PublicServiceApplicationTests {
 
 	@Test
